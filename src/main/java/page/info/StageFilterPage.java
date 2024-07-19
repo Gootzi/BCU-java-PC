3169a78b1e59d0f12d6e488455eacaa85b5b3b62
iRed <foodrickme@gmail.com>
2024-07-18 21:43:07 -0700

    add pinpoint to stage filter page

--- src/main/java/page/info/StageFilterPage.java
+++ src/main/java/page/info/StageFilterPage.java
@@ -1,7 +1,9 @@
 package page.info;
 
+import common.util.stage.MapColc;
 import common.util.stage.Stage;
 import main.MainBCU;
+import page.JBTN;
 import page.JTF;
 import page.Page;
 import utilpc.UtilPC;
@@ -18,6 +20,7 @@ public class StageFilterPage extends StagePage {
 	private final Stage[] stages;
 	private final JList<Stage> jlst = new JList<>();
 	private final JScrollPane jspst = new JScrollPane(jlst);
+	private final JBTN pins = new JBTN(0, "pinpoint");
 
 	public StageFilterPage(Page p, List<Stage> ls) {
 		super(p);
@@ -30,8 +33,9 @@ public class StageFilterPage extends StagePage {
 	protected void resized(int x, int y) {
 		super.resized(x, y);
 
-		set(srch, x, y, 400, 500, 300, 50);
-		set(jspst, x, y, 400, 550, 300, 650);
+		set(srch, x, y, 400, 500, 400, 50);
+		set(jspst, x, y, 400, 550, 400, 650);
+		set(pins, x, y, 600, 1200, 200, 50);
 		set(strt, x, y, 400, 0, 300, 50);
 	}
 
@@ -67,13 +71,18 @@ public class StageFilterPage extends StagePage {
 			jlst.setSelectedIndex(Math.max(filtered.indexOf(curr), 0));
 		});
 
+		pins.setLnr(x -> {
+			Stage st = jlst.getSelectedValue();
+			if (st != null)
+				changePanel(new StageViewPage(this, MapColc.values(), st));
+		});
 	}
 
 	private void ini() {
 		add(srch);
 		add(jspst);
+		add(pins);
 		addListeners();
-
 	}
 
 }
